e8c7f52577a1508e03d924bf26eb339dba95b9dd
csamar117 <csabaimartin117@gmail.com>
2023-03-11 13:46:51 +0100

    uibuild

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
-app.use(express.static('build'))
 
 let persons = [
   {
